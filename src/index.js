21dc6471683bf0ae2554d11a5726370aaa7bd31a
mannals <annamalassu@gmail.com>
2023-10-29 11:11:17 +0200

    delete request

--- src/index.js
+++ src/index.js
@@ -1,13 +1,33 @@
 import http from 'http';
+import { getItems, getItemsById, postItem } from './items.js';
 const hostname = '127.0.0.1';
 const port = 3000;
 
+
 const server = http.createServer((req, res) => {
-  console.log('request ', req.method, req.url);
-  // TODO: test method, url and generate response accordingly
-  // use e.g. if-else
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end('<h1>Welcome to my REST API!</h1>');
+  console.log('request', req.method, req.url);
+
+  const {method, url} = req;
+  const reqParts = url.split('/');
+
+  if (method === 'GET' && url === '/') {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write('<h1>Welcome to my API</h1>');
+    res.write('<p>Ebin</p>');
+    res.end();
+  } else if (method === 'GET' && reqParts[1] === 'items' && url.split('/')[2]) {
+    console.log('GETting item with ID', reqParts[2]);
+    getItemsById(res, reqParts[2]);
+  } else if (method === 'GET' && reqParts[1] == 'items') {
+    console.log('GETting items');
+    getItems(res);
+  } else if (method === 'POST') {
+    console.log('Posting..');
+    postItem(req, res);
+  } else {
+    res.writeHead(404, {'Content-Type': 'application/json'});
+    res.end('{"message": "404 resource not found!"}');
+  }
 });
 
 server.listen(port, hostname, () => {
---
+++ src/items.js
@@ -0,0 +1,62 @@
+
+const items = [
+    {id: 5, name: 'porkkana'},
+    {id: 6, name: 'omena'},
+    {id: 19, name: 'appelsiini'}
+];
+  
+const getItems = (res) => {
+    res.writeHead(200, {'Content-Type': 'application/json'});
+    const jsonItems = JSON.stringify(items);
+    res.end(`{"message": "All items", "items": ${jsonItems}}`)
+}
+
+const getItemsById = (res, id) => {
+    // TODO: if item with id exists send it, otherwise 404
+    console.log("getItemsById", id);
+    const item = items.find((element) => element.id == id);
+    if (item) {
+        res.writeHead(200, {'Content-Type': 'application/json'});
+        res.end(JSON.stringify(item));
+    } else {
+        res.writeHead(404, {'Content-Type': 'application/json'});
+        res.end('{"message": "Item not found!"}'); 
+    }
+} 
+
+const postItem = (req, res) => {
+
+    let body = [];
+    req.on('error', (err) => {
+        console.error(err);
+    }).on('data', (chunk) => {
+        body.push(chunk);
+    }).on('end', () => {
+        body = Buffer.concat(body).toString();
+        console.log('req body', body);
+        body = JSON.parse(body);
+        if (!body.name) {
+            res.writeHead(400, {'Content-Type': 'application/json'});
+            res.end('{"message": "Missing data!"}');
+
+        }
+        const newId = items[items.length-1].id + 1;
+        items.push({id: newId, name: body.name})
+        res.writeHead(201, {"Content-Type": "application0/json"});
+        res.end('{"message": "New item added."}');
+    });
+}
+
+const deleteItem = (res, id) => {
+    console.log('deleteItem', id);
+    const item = items.find((el) => el.id == id);
+    if (item) {
+        res.writeHead(204, {'Content-Type': 'application/json'});
+        res.end(`{"message": "Item with the ID of ${id} successfully deleted."}`);
+    } else {
+        res.writeHead(404, {'Content-Type': 'application/json'});
+        res.end('{"message": "Item not found!"}');
+    }
+}
+
+export {getItems, getItemsById, postItem};
